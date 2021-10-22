590653d43eb6ca1095d393ba77ae8dd59393e0a5
fwg <adf0001@163.com>
2021-10-22 20:26:13 +0800

    update: change to use code from module prototype-clone

--- combine-config.js
+++ combine-config.js
@@ -4,43 +4,70 @@
 
 var call_log_func = require("call-log-func");
 var is_simple_object = require("is-simple-object");
+var prototype_clone = require("prototype-clone");
 
-//var combine_config = function (baseConfig, subConfig [, logFunc [, logPath]] )
-var combine_config = function (baseConfig, subConfig, logFunc, logPath) {
-	var target = Object.create(baseConfig);
+var createRef = prototype_clone.createRef;
 
-	var i, bi, si;
-	for (i in subConfig) {
-		si = subConfig[i];
+//re-write prototype_clone.copySingle()
+var copySingle = function (target, source, refMap, logFunc, logPath) {
+	if (!refMap) refMap = new Map();		//map the original to the derived
+	if (!target) target = createRef(source, refMap);
+
+	var i, ti, si, si_simple, i2;
+	for (i in source) {
+		si = source[i];
 
 		//forcely replace
 		if (typeof i === "string" && i.charAt(0) == "!") {
-			target[i.slice(1)] = si;
-			call_log_func(logFunc, (logPath || "") + "." + i.slice(1) + ", \t\t replaced forcely");
+			i2 = i.slice(1);
+			call_log_func(logFunc, logPath + "." + i2 + ", \t\t replaced forcely");
+
+			if (is_simple_object(si)) target[i2] = copySingle(null, si, refMap, logFunc, logPath + "." + i2);
+			else target[i2] = si;
+
 			continue;
 		}
 
-		//if not exist
-		if (!(i in baseConfig)) {
-			target[i] = si;
-			call_log_func(logFunc, (logPath || "") + "." + i + ", \t\t added");
-			continue;
+		ti = target[i];
+
+		if ((si_simple = is_simple_object(si)) && is_simple_object(ti)) {
+			//deep copy
+			if (Object.prototype.hasOwnProperty.call(target, i)) {
+				if (!refMap.has(si) || refMap.get(si) !== ti) copySingle(ti, si, refMap, logFunc, logPath + "." + i);
+				//else console.log("skip already copied, " + i);	//skip already copied
+			}
+			else {
+				target[i] = refMap.has(si) ?
+					refMap.get(si) :		//copy already copied
+					copySingle(null, si, refMap, logFunc, logPath + "." + i);
+			}
 		}
-
-		bi = baseConfig[i];
-		if (bi === si) continue;
-
-		if (!is_simple_object(si) || !is_simple_object(bi)) {
-			target[i] = si;
-			call_log_func(logFunc, (logPath || "") + "." + i + ", \t\t replaced");
-			continue;
+		else {
+			//direct copy
+			if (si_simple) target[i] = createRef(si, refMap);
+			else if (ti !== si) {
+				call_log_func(logFunc, (logPath || "") + "." + i + ", \t\t " + ((typeof ti === "undefined") ? "added" : "replaced"));
+				target[i] = si;
+			}
+			//else { }	//skip same
 		}
-
-		target[i] = combine_config(bi, si, logFunc, (logPath || "") + "." + i);
 	}
+	return target;
+}
+
+//var combine_config = function (baseConfig, subConfig [, logFunc ] )
+var combine_config = function (baseConfig, subConfig, logFunc) {
+	var refMap = new Map();		//map the original to the derived
+
+	var target = null;
+	target = copySingle(target, baseConfig, refMap, logFunc, "");
+	target = copySingle(target, subConfig, refMap, logFunc, "");
+
 	return target;
 }
 
 //module
 
-module.exports = combine_config;
+module.exports = exports = combine_config;
+
+exports.copySingle = copySingle;	//export sub tool
